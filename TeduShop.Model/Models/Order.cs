d2f150e98010a182a0d55fbd73d9ebd848d01fd3
Duy Dinh <dinhquangduynt@gmail.com>
2020-04-01 22:34:02 +0700

    add domail entities for project

---
+++ TeduShop.Model/Models/Order.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Orders")]
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        public string CustomerName { get; set; }
+        [Required]
+        public string CustomerAddress { get; set; }
+        [Required]
+        public string CustomerEmail { get; set; }
+        [Required]
+        public string CustomerMobile { get; set; }
+        
+        public string CustomerMessage { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string PaymentMethod { get; set; }
+        [Required]
+        public string PaymentStatus { get; set; }
+        public bool Status { get; set; }
+
+        public virtual IEnumerable<OrderDetail> OrderDetails { get; set; }
+    }
+}
